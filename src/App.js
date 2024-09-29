f6281dc17cc90bd79692811474b1f3985eae8754
sivakumar51 <sivakumarnallasamy24@gmail.com>
2024-09-30 02:03:40 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
   return (
     <div className="App">
-      <h2>ghhg</h2>
+      <TodoWrapper  />
     </div>
   );
 }
